ea95f0f5cfc781272fd543807682296289b45584
齐龙 <qilong@leying365.com>
2017-01-13 17:52:31 +0800

    打算集成zipkin

---
+++ ql-cloud-commons/ql-cloud-commons-zipkin/src/main/java/com/ql/cloud/commons/zipkin/App.java
@@ -0,0 +1,12 @@
+package com.ql.cloud.commons.zipkin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        // 教程链接 https://yq.aliyun.com/articles/60165
+        System.out.println("Hello World!");
+    }
+}
